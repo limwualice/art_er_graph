0933e0ec311185faba919c5bb73622388db273aa
Alice Lim <limwualice@gmail.com>
2025-04-16 10:49:36 -0700

    added more to cleaning script

--- scripts/clean_getty.py
+++ scripts/clean_getty.py
@@ -18,6 +18,19 @@ def delete_missing_cols(df):
 
 
     
+#####################################################################################
+###This function deletes the columns that only contain one value, for each dataframe.
+#####################################################################################
+
+def delete_columns_one_value(df):
+    to_drop=[]
+    for j in df.columns:
+        if len(df[j].value_counts().values)==1:
+            to_drop.append(j)
+    df=df.drop(to_drop, axis=1)
+    return df
+
+    
 ##################################################################################
 ### This function checks the top 5 value counts for every column in the dataframe. 
 ##################################################################################
@@ -52,11 +65,22 @@ for i in range(1,n+1):
 #######################################################
 ###This deletes the missing columns for each dataframe.
 #######################################################
-
+print('********************** deleting nan columns **********************')
 for i in range(1,n+1):
-    print(i, len(dataframes[f'df{i}'].columns))
+    print(f'dataframe {i} starts with ', len(dataframes[f'df{i}'].columns), ' columns')
     dataframes[f'df{i}']=delete_missing_cols(dataframes[f'df{i}'])
-    print(i, len(dataframes[f'df{i}'].columns)) 
+    print('after deleting: ', len(dataframes[f'df{i}'].columns), ' columns') 
+
+
+    
+#########################################################
+###This deletes the one value columns for each dataframe.
+#########################################################
+print('********************** deleting 1-value columns **********************')
+for i in dataframes:
+    dataframes[i]=delete_columns_one_value(dataframes[i])
+    print(f'dataframe {i} after deleting: ', len(dataframes[i].columns.to_list()))
+
 
 
     
@@ -66,7 +90,7 @@ for i in range(1,n+1):
 
 clean_file_location = os.path.join(os.path.dirname(current_dir), 'clean_getty_csvs')
 for i in range(1, n+1):
-    dataframes[f'df{i}'].to_csv(clean_file_location+f'/df{i}.csv')
+    dataframes[f'df{i}'].to_csv(clean_file_location+f'/df{i}.csv', index=False)
 
 
 
